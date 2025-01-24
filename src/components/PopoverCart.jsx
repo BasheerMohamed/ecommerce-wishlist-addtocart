8652d4a80fe55e14e633065e44e61346d6e17bae
BasheerMohamed <Basheer_Mohamed@proton.me>
2025-01-24 21:52:14 +0530

    commit

--- src/components/PopoverCart.jsx
+++ src/components/PopoverCart.jsx
@@ -4,26 +4,6 @@ import { useSelector } from 'react-redux'
 import { selectCart, selectCartCount } from '../Redux/slice'
 import { useNavigate } from 'react-router-dom'
 
-const products = [
-  {
-    id: 1,
-    name: 'Throwback Hip Bag',
-    href: '#',
-    color: 'Salmon',
-    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
-    imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
-  },
-  {
-    id: 2,
-    name: 'Medium Stuff Satchel',
-    href: '#',
-    color: 'Blue',
-    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
-    imageAlt:
-      'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
-  },
-]
-
 const PopoverCart = () => {
 
   const cartCount = useSelector(selectCartCount);
